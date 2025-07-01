ce5988805321056630346857dc0e7a795a8c77ed
sgembal <sgembal@craftware.biz>
2025-07-01 21:07:38 +0200

    refactor

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -2,7 +2,14 @@ import { test, expect } from '@playwright/test';
 
 test.describe('User login to demo bank', () => {
   // Arrange
+
+
+  //hook --> 
+  test.beforeEach(async ({ page }) =>{
   const url = 'https://demo-bank.vercel.app/';
+  await page.goto(url);
+  })
+
   const userPassword = '08987654321';
   const userId = 'testerHi';
   const expectedUsername = 'Jan Demobankowy';
@@ -12,17 +19,14 @@ test.describe('User login to demo bank', () => {
 
   test('sucesfully login with correct credential', async ({ page }) => {
     // Act
-    await page.goto(url);
-    await page.getByTestId('login-input').fill(userId);
+     await page.getByTestId('login-input').fill(userId);
     await page.getByTestId('password-input').fill(userPassword);
     await page.getByTestId('login-button').click();
-
     // Assert
     await expect(page.getByTestId('user-name')).toHaveText(expectedUsername);
   });
 
   test('unsucesfull login with to short username', async ({ page }) => {
-    await page.goto(url);
     await page.getByTestId('login-input').fill(userId);
     await page.getByTestId('password-input').click();
 
@@ -32,7 +36,6 @@ test.describe('User login to demo bank', () => {
   });
 
   test('unsucesfull login with to short password', async ({ page }) => {
-    await page.goto(url);
     await page.getByTestId('login-input').fill(userId);
     await page.getByTestId('password-input').fill(incorrectUserPassword);
     await page.getByTestId('password-input').blur();
--- tests/pulpit.spec.ts
+++ tests/pulpit.spec.ts
@@ -2,7 +2,11 @@ import { expect, test } from '@playwright/test';
 
 test.describe('Pulpit test', async () => {
   //  Arrange
-  const url = 'https://demo-bank.vercel.app/';
+  test.beforeEach(async ({ page }) => {
+    const url = 'https://demo-bank.vercel.app/';
+    await page.goto(url);
+  });
+
   const userPassword = '08987654321';
   const userId = 'testerHi';
   const reciverId = '2';
@@ -14,17 +18,14 @@ test.describe('Pulpit test', async () => {
 
   // Act
   test('send money to someone', async ({ page }) => {
-    await page.goto(url);
     await page.getByTestId('login-input').fill(userId);
     await page.getByTestId('password-input').fill(userPassword);
     await page.getByTestId('login-button').click();
-
     await page.locator('#widget_1_transfer_receiver').selectOption(reciverId);
     await page.locator('#widget_1_transfer_amount').fill(transferAmount);
     await page.locator('#widget_1_transfer_title').fill(transferTitile);
     await page.locator('#execute_btn').click();
     await page.getByTestId('close-button').click();
-
     // Assert
     await expect(page.locator('#show_messages')).toHaveText(
       `Przelew wykonany! ${expectedTransferReciver} - ${transferAmount},00PLN - ${transferTitile}`,
@@ -34,7 +35,6 @@ test.describe('Pulpit test', async () => {
   });
 
   test('sucessful mobile top-up', async ({ page }) => {
-    await page.goto(url);
     await page.getByTestId('login-input').fill(userId);
     await page.getByTestId('password-input').fill(userPassword);
     await page.getByTestId('login-button').click();
@@ -43,9 +43,7 @@ test.describe('Pulpit test', async () => {
     await page.locator('#uniform-widget_1_topup_agreement span').click();
     await page.getByRole('button', { name: 'doładuj telefon' }).click();
     await page.getByTestId('close-button').click();
-    await expect(page.getByTestId('message-text')).toHaveText(
-      expectedMessage,
-    );
+    await expect(page.getByTestId('message-text')).toHaveText(expectedMessage);
     // albo za pomocą lokatora page.locator(#show_messages).toHaveText('Doładowanie wykonane! 12,00PLN na numer 500 xxx xxx')
   });
 });
