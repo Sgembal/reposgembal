01896e71f154c62a4d5a409ccbf18e2645ab13d2
sgembal <sgembal@craftware.biz>
2025-07-09 23:26:00 +0200

    testPlik

--- pages/login.page.ts
+++ pages/login.page.ts
@@ -1,4 +1,5 @@
 import { Locator, Page } from '@playwright/test';
+import { SideMenuComponent } from '../components/side-menu-component';
 
 export class LoginPage {
   loginInput: Locator;
@@ -6,12 +7,21 @@ export class LoginPage {
   loginButton: Locator;
   loginError: Locator;
   passwordError: Locator;
+  sideMenuComponent: SideMenuComponent;
 
   constructor(private page: Page) {
+    this.sideMenuComponent = new SideMenuComponent(this.page);
+
     this.loginInput = this.page.getByTestId('login-input');
     this.passwordInput = this.page.getByTestId('password-input');
     this.loginButton = this.page.getByTestId('login-button');
     this.loginError = this.page.getByTestId('error-login-id');
     this.passwordError = this.page.getByTestId('error-login-password');
+  }
+
+  async login(userId: string, userPassword: string): Promise<void> {
+    await this.loginInput.fill(userId);
+    await this.passwordInput.fill(userPassword);
+    await this.loginButton.click();
   }
 }
